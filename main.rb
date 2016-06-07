64b3c4d0b504ab9b4e634f415867b6a96188ee2d
Cody Loyd <codyloyd@gmail.com>
2016-06-07 13:30:10 -0500

    update

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sass'
+require  './song'
 require 'sinatra/reloader'
 
 
@@ -15,6 +16,42 @@ end
 get '/contact' do
 	@title = "Contact"	
 	erb :contact
+end
+
+get '/songs' do
+	@songs = Song.all
+	erb :songs
+end
+
+post '/songs' do 
+	song = Song.create(params[:song])
+	redirect to("/songs/#{song.id}")
+end
+
+get '/songs/new' do
+	@song = Song.new
+	erb :new_song
+end
+
+get '/songs/:id' do
+	@song = Song.get(params[:id])
+	erb :show_song
+end
+
+put '/songs/:id' do
+	song = Song.get(params[:id])
+	song.update(params[:song])
+	redirect to("/songs/#{song.id}")
+end
+
+get '/songs/:id/edit' do 
+	@song = Song.get(params[:id])
+	erb :edit_song
+end
+
+delete '/songs/:id' do
+	Song.get(params[:id]).destroy
+	redirect to('/songs')
 end
 
 not_found do 
