56fa8f555fe9d3d0db2e50dc05ac4264a21ff0e3
Cody Loyd <codyloyd@gmail.com>
2016-06-07 23:37:59 -0500

    added some stuff

--- main.rb
+++ main.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'sass'
+require 'slim'
 require  './song'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 
+configure do
+	enable :sessions
+	set :username, 'cody'
+	set :password, 'password'
+end
 
 get '/' do
 	erb :home
@@ -29,6 +35,7 @@ post '/songs' do
 end
 
 get '/songs/new' do
+	halt(401, 'Not Authorized') unless session[:admin] 
 	@song = Song.new
 	erb :new_song
 end
@@ -45,13 +52,33 @@ put '/songs/:id' do
 end
 
 get '/songs/:id/edit' do 
+	halt(401, 'Not Authorized') unless session[:admin] 
 	@song = Song.get(params[:id])
 	erb :edit_song
 end
 
 delete '/songs/:id' do
+	halt(401, 'Not Authorized') unless session[:admin] 
 	Song.get(params[:id]).destroy
 	redirect to('/songs')
+end
+
+get '/login' do 
+	slim :login	
+end
+
+post '/login' do 
+	if params[:username] == settings.username && params[:password] == settings.password
+		session[:admin] = true
+		redirect to('/songs')
+	else
+		slim :login
+	end
+end
+
+get '/logout' do 
+	session.clear
+	redirect to('/login')	
 end
 
 not_found do 
