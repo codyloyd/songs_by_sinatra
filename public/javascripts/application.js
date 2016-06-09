17fa4900585cce7e19eb9a46549eb695a361a38e
Cody Loyd <codyloyd@gmail.com>
2016-06-08 19:14:20 -0500

    did a like button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 	$("#released_on").datepicker( {changeYear: true, yearRange: "1940:2000"});
+	
+	return $('#like input').click(function(event) {
+    event.preventDefault();
+    return $.post($('#like form').attr('action'), function(data) {
+      return $('#like p').html(data).effect('highlight', {
+        color: '#fcd'
+      });
+    });
 });
 
